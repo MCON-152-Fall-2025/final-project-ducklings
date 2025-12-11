883ca5837b1a20791c341ea45467fc3439871b0a
Why3v3rN07 <hfogarty@student.touro.edu>
2025-12-11 16:31:40 -0500

    RS1F2-249 break stuff

--- recipeshare/src/test/java/com/mcon152/recipeshare/RecipeshareApplicationTests.java
+++ recipeshare/src/test/java/com/mcon152/recipeshare/RecipeshareApplicationTests.java
@@ -3,11 +3,14 @@ package com.mcon152.recipeshare;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class RecipeShareApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(false);
     }
 
 }
